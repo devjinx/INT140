e926d99523771a92f981c43bf44dbbc45c4b41fd
devjinx <t.chareonlertkamol@gmail.com>
2024-12-11 21:37:57 +0700

    update test

--- Final_Project/test/test_calculator.py
+++ Final_Project/test/test_calculator.py
@@ -2,12 +2,14 @@ import unittest
 from calculator import Calculator
 
 class TestCalculator(unittest.TestCase):
-    def test_calculate_total(self):
-        menu = {"Burger": 5.99, "Fries": 2.99}
-        total = Calculator.calculate_total(["Burger", "Fries"], menu)
-        self.assertEqual(total, 8.98)
+    def test_calculate_total_with_quantity(self):
+        menu = {"Burger": 59.99, "Fries": 29.99}
+        order = {"Burger": 2, "Fries": 1}
+        total = Calculator.calculate_total_with_quantity(order, menu)
+        self.assertEqual(total, 149.97)
 
     def test_item_not_in_menu(self):
-        menu = {"Burger": 5.99}
+        menu = {"Burger": 59.99}
+        order = {"Pizza": 1}
         with self.assertRaises(ValueError):
-            Calculator.calculate_total(["Pizza"], menu)
+            Calculator.calculate_total_with_quantity(order, menu)
--- Final_Project/test/test_menu.py
+++ Final_Project/test/test_menu.py
@@ -1,14 +1,14 @@
 import unittest
-from Final_Project.test.test_menu import Menu
+from menu import Menu
 
 class TestMenu(unittest.TestCase):
     def test_add_menu_item(self):
         menu = Menu()
-        menu.add_menu_item("Burger", 5.99)
-        self.assertEqual(menu.get_menu(), {"Burger": 5.99})
+        menu.add_menu_item("Burger", 59.99)
+        self.assertEqual(menu.get_menu(), {"Burger": 59.99})
 
     def test_add_duplicate_item(self):
         menu = Menu()
-        menu.add_menu_item("Burger", 5.99)
+        menu.add_menu_item("Burger", 59.99)
         with self.assertRaises(ValueError):
-            menu.add_menu_item("Burger", 6.99)
+            menu.add_menu_item("Burger", 69.99)
